3b1e6755a70a069995f807b041305afa58d0030f
lucas.caminha <lucas_santoscaminha@hotmail.com>
2026-07-16 13:20:37 -0300

    feat(trainers): add temple vortex and monk spawns

--- data-otservbr-global/scripts/custom/movement_eclipse_room_teleports.lua
+++ data-otservbr-global/scripts/custom/movement_eclipse_room_teleports.lua
@@ -5,6 +5,7 @@ local teleports = {
 	{ name = "Thais temple entrance to Eclipse room", from = Position(32365, 32236, 7), to = Position(1045, 1037, 7) },
 	{ name = "Boss room entrance", from = Position(32373, 32236, 7), to = Position(32317, 32258, 15) },
 	{ name = "Boss room exit", from = Position(32313, 32260, 15), to = Position(32369, 32241, 7) },
+	{ name = "Temple water vortex to trainers", from = Position(32365, 32239, 7), to = Position(1152, 1037, 4) },
 	{ name = "Dawnport start portal to Eclipse room", from = Position(32068, 31898, 6), to = Position(1045, 1037, 7) },
 	{ name = "Grand Master Oberon boss entrance", from = Position(32316, 32257, 15), to = Position(33362, 31339, 7) },
 	{ name = "Grand Master Oberon boss exit", from = Position(33361, 31338, 7), to = Position(32315, 32260, 15) },
